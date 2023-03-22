458107eb80846c1b60f64825af4a423cc412d162
sergiojulio <sergiojulio@gmail.com>
2023-03-22 17:24:13 -0300

    Files

---
+++ airflow/dags/dbt_example.py
@@ -0,0 +1,48 @@
+import os
+import json
+import pendulum
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
+HOME = "/home/sergio/dev/python/music-analysis-pipeline" # os.environ["HOME"] # retrieve the location of your home folder
+dbt_path = os.path.join(HOME, "/transform/dbt/my_project") # path to your dbt project
+manifest_path = os.path.join(dbt_path, "/target/manifest.json") # path to manifest.json
+
+with open(manifest_path) as f: # Open manifest.json
+  manifest = json.load(f) # Load its contents into a Python Dictionary
+  nodes = manifest["nodes"] # Extract just the nodes
+
+# Build an Airflow DAG
+with DAG(
+  dag_id="dbt_example", # The name that shows up in the UI
+  start_date=pendulum.today(), # Start date of the DAG
+  catchup=False,
+) as dag:
+
+  # Create a dict of Operators
+  dbt_tasks = dict()
+  for node_id, node_info in nodes.items():
+      dbt_tasks[node_id] = BashOperator(
+          task_id=".".join(
+              [
+                  node_info["resource_type"],
+                  node_info["package_name"],
+                  node_info["name"],
+              ]
+          ),
+          bash_command=f"cd {dbt_path}" # Go to the path containing your dbt project
+          + ' && eval "\$(pyenv init -)"' # Load Pyenv
+          + ' && eval "\$(pyenv virtualenv-init -)"' # Load Pyenv Virtualenv
+          + " && pyenv activate demo_dbt" # Activate the dbt virtual environment
+          + f" && dbt run --models {node_info['name']}", # run the model!
+      )
+
+  # Define relationships between Operators
+  for node_id, node_info in nodes.items():
+      upstream_nodes = node_info["depends_on"]["nodes"]
+      if upstream_nodes:
+          for upstream_node in upstream_nodes:
+              dbt_tasks[upstream_node] >> dbt_tasks[node_id]
+
+if __name__ == "__main__":
+  dag.cli()
