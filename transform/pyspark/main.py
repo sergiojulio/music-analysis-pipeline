4c66fc0c59bf3b0deaa872cecb51ac9e0bfad3c2
Sergio Sierra <sergiojulio@outlook.com>
2023-03-27 21:32:11 -0300

    added files

---
+++ transform/pyspark/main.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created 2022-03-17
+
+@author: Sergio Sierra
+
+"""
+import shutil
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as func  # este alias es para evitar sobre escritura de funciones
+from pyspark.sql.types import IntegerType, StringType
+
+spark = SparkSession.builder.appName("homologacion").getOrCreate()
+sc = spark.sparkContext
+
+df = spark.read.option("header", True).format("csv").option("delimiter", ',').load("/home/sergio/dev/python/music-analysis-pipeline/inputs/scrobbles-radioheadve-1679014882.csv")
+
+df.groupBy('artist').count().orderBy('count', ascending=False).show()
