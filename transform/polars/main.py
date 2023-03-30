1c538fe8396c879fcbd43ce838b263e3ec4ffdff
Sergio Sierra <sergiojulio@outlook.com>
2023-03-29 21:20:59 -0300

    setting up polars

--- transform/polars/main.py
+++ transform/polars/main.py
@@ -1,10 +1,5 @@
-import pandas as pd
-import numpy as np
+import polars as pl
 
-df = pd.read_csv('../../inputs/scrobbles-radioheadve-1679014882.csv', index_col='uts')
+df = pl.read_csv('../../inputs/scrobbles-radioheadve-1679014882.csv', parse_dates=False)
 
-# print(df['artist'].value_counts().head(10))
-# df.groupBy('artist').count().orderBy('count', ascending=False).show()
-
-
-print(df.iloc[1])
+print(df.head(5))
