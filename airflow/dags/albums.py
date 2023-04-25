2f2ce8c8726ce2657dcf5b910722a961555ff144
sergiojulio <sergiojulio@gmail.com>
2023-04-24 20:48:23 -0400

    PEP 8 fixes

--- airflow/dags/albums.py
+++ airflow/dags/albums.py
@@ -1,26 +1,25 @@
 from airflow.models import DAG
 from airflow.operators.bash import BashOperator
 from airflow.utils.dates import days_ago
-from datetime import datetime
 
 
 args = {
     'owner': 'Serginho',
-    'start_date': days_ago(1) # make start date in the past
+    'start_date': days_ago(1)
 }
 
-#defining the dag object
+# defining the dag object
 dag = DAG(
     dag_id='extract-albums',
     default_args=args,
-    schedule_interval=None #to make this workflow happen every day
+    schedule_interval=None
 )
 
-#todo path env
 with dag:
     step1 = BashOperator(
                 task_id='step1', 
-                bash_command='source /home/sergio/dev/python/music-analysis-pipeline/.env/bin/activate && python /home/sergio/dev/python/music-analysis-pipeline/airflow/dags/extract/albums.py',
+                bash_command='source /home/sergio/dev/python/music-analysis-pipeline/.env/bin/activate && python '
+                             '/home/sergio/dev/python/music-analysis-pipeline/airflow/dags/extract/albums.py',
             )
     
     step2 = BashOperator(
