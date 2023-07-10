4b09e79b322e2e3934f7ea410c4f339995b4281f
sergiojulio <sergiojulio@gmail.com>
2023-07-10 19:38:42 -0400

    Artists files added

---
+++ src/artists/pandas/artists.py
@@ -0,0 +1,33 @@
+import sqlite3
+import pandas as pd
+
+# env path db, 
+# path raw file name, 
+# arg table, 
+
+
+def pandas_sqlite_csv():
+    conn = sqlite3.connect('/home/sergio/dev/python/music-analysis-pipeline/inputs/spotify.sqlite')
+    # Could not decode to UTF-8 column 'name'
+    conn.text_factory = bytes
+    # cursor = conn.cursor()
+    # weirdos chars
+    # conn.text_factory = str
+    table = pd.read_sql('SELECT * FROM albums', conn)
+    # """
+    table['id'] = table['id'].str.decode('utf8')
+    # UnicodeDecodeError: 'utf-8' codec can't decode byte 0xed in position 16: invalid continuation byte
+    table['name'] = table['name'].str.decode('utf8', errors='ignore')
+    table['album_group'] = table['album_group'].str.decode('utf8')
+    table['album_type'] = table['album_type'].str.decode('utf8')
+    # Dq...
+    table.loc[table['release_date'] == 0, 'release_date'] = -2208902400000
+    table.loc[table['release_date'] < -2208902400000, 'release_date'] = -2208988800000
+
+    # """
+    table.to_csv('/home/sergio/dev/python/music-analysis-pipeline/datalake/raw/albums.csv',
+                 index=False, encoding='utf-8')
+
+
+if __name__ == '__main__':
+    pandas_sqlite_csv()
