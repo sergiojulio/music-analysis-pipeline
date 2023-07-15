07a9deb508dcbed988d048c58f141ed4c67d12ad
sergiojulio <sergiojulio@gmail.com>
2023-07-14 20:22:13 -0400

    artists pipeline

--- src/albums/pyspark/albums.py
+++ src/albums/pyspark/albums.py
@@ -12,30 +12,29 @@ def get_current_datetime():
 udf_get_date = func.udf(get_current_datetime, returnType=StringType())
 """
 
-
 if __name__ == "__main__":
-
     # sc = SparkContext(appName="CSV2Parquet")
     # sqlContext = SQLContext(sc)
     spark = SparkSession.builder.appName("homologacion").getOrCreate()
     sc = spark.sparkContext
 
     schema = StructType([
-                StructField("id", StringType(), True),
-                StructField("name", StringType(), True),
-                StructField("album_group", StringType(), True),
-                StructField("album_type", StringType(), True),
-                StructField("release_date", StringType(), True),
-                StructField("popularity", DecimalType(12, 0), True)
-            ])
-    
+        StructField("id", StringType(), True),
+        StructField("name", StringType(), True),
+        StructField("album_group", StringType(), True),
+        StructField("album_type", StringType(), True),
+        StructField("release_date", StringType(), True),
+        StructField("popularity", DecimalType(12, 0), True)
+    ])
+
     dirname = os.path.dirname(os.path.abspath(__file__))
 
     # csvfilename = os.path.join(dirname, 'Temp.csv')
 
-    df = spark.read.option("header", True).format("csv").schema(schema).option("delimiter", ',').load("/home/sergio/dev/python/music"
-                                                                                       "-analysis-pipeline/datalake"
-                                                                                       "/raw/albums.csv")
+    df = spark.read.option("header", True).format("csv").schema(schema).option("delimiter", ',').load(
+        "/home/sergio/dev/python/music"
+        "-analysis-pipeline/datalake"
+        "/raw/albums.csv")
 
     # change column type
     """
@@ -51,13 +50,15 @@ if __name__ == "__main__":
     systems that use Proleptic Gregorian calendar.    
     """
 
-    df = df.withColumn('process_date', func.lit(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")).cast(TimestampType()))
+    df = df.withColumn('process_date',
+                       func.lit(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")).cast(TimestampType()))
     df = df.withColumn("_release_date", func.to_timestamp(func.col("release_date") / 1000).cast(TimestampType()))
     df = df.drop('release_date')
     df = df.withColumnRenamed("_release_date", "release_date")
 
     parquetfilename = os.path.join(dirname, 'output.parquet')
 
-    df.write.mode('overwrite').parquet('/home/sergio/dev/python/music-analysis-pipeline/datalake/parquet/albums.parquet')
+    df.write.mode('overwrite').parquet(
+        '/home/sergio/dev/python/music-analysis-pipeline/datalake/parquet/albums.parquet')
 
     df.show()
--- src/artists/pandas/artists.py
+++ src/artists/pandas/artists.py
@@ -15,15 +15,10 @@ def pandas_sqlite_csv():
     # conn.text_factory = str
     table = pd.read_sql('SELECT * FROM artists', conn)
     # """
-    table['id'] = table['id'].str.decode('utf8')
-    # UnicodeDecodeError: 'utf-8' codec can't decode byte 0xed in position 16: invalid continuation byte
     table['name'] = table['name'].str.decode('utf8', errors='ignore')
-    table['album_group'] = table['album_group'].str.decode('utf8')
-    table['album_type'] = table['album_type'].str.decode('utf8')
-    # Dq...
-    table.loc[table['release_date'] == 0, 'release_date'] = -2208902400000
-    table.loc[table['release_date'] < -2208902400000, 'release_date'] = -2208988800000
-
+    table['id'] = table['id'].str.decode('utf8')
+    table['popularity'] = table['popularity']
+    table['followers'] = table['followers']
     # """
     table.to_csv('/home/sergio/dev/python/music-analysis-pipeline/datalake/raw/artists.csv',
                  index=False, encoding='utf-8')
