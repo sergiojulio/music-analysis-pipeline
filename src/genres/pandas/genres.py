56d3728635e9ce8df169a38a46774eced400f224
sergiojulio <sergiojulio@gmail.com>
2023-10-02 21:04:52 -0300

    genres bugs

--- src/genres/pandas/genres.py
+++ src/genres/pandas/genres.py
@@ -13,7 +13,7 @@ def pandas_sqlite_csv():
     # cursor = conn.cursor()
     # weirdos chars
     # conn.text_factory = str
-    table = pd.read_sql('SELECT id, name, album_group, album_type, release_date, popularity FROM tracks', conn)
+    table = pd.read_sql('SELECT id FROM genres', conn)
     # ""
     table['id'] = table['id'].str.decode('utf8')
     # UnicodeDecodeError: 'utf-8' codec can't decode byte 0xed in position 16: invalid continuation byte
