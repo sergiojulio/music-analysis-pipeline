31e4cd9dad973a51d31b7a39b8a106b1275a2ffc
sergiojulio <sergiojulio@gmail.com>
2023-10-20 20:26:31 -0300

    genres comments

--- src/genres/pandas/genres.py
+++ src/genres/pandas/genres.py
@@ -26,7 +26,6 @@ def pandas_sqlite_csv():
     table.loc[table['release_date'] < -2208902400000, 'release_date'] = -2208988800000
     # aqui hay que generar un hash que sea el id
     # luego en tabla link artist_genre hay que reemplazar el name por el id
-    # hay que crear un new file
     table['popularity'] = table['popularity']
     # """
     table.to_csv('/home/sergio/dev/python/music-analysis-pipeline/datalake/raw/tracks.csv',
