f4d53d66a95adc2bb3b997199e12fd6f44a93e7f
sergiojulio <sergiojulio@gmail.com>
2023-10-22 20:48:59 -0300

    genres pipeline

--- src/genres/pandas/genres.py
+++ src/genres/pandas/genres.py
@@ -15,20 +15,11 @@ def pandas_sqlite_csv():
     # conn.text_factory = str
     table = pd.read_sql('SELECT id FROM genres', conn)
     # ""
-    table['id'] = table['id'].str.decode('utf8')
-    # UnicodeDecodeError: 'utf-8' codec can't decode byte 0xed in position 16: invalid continuation byte
-    #
-    table['name'] = table['name'].str.decode('utf8', errors='ignore')
-    table['album_group'] = table['album_group'].str.decode('utf8')
-    table['album_type'] = table['album_type'].str.decode('utf8')
-    # Dq..
-    table.loc[table['release_date'] == 0, 'release_date'] = -2208902400000
-    table.loc[table['release_date'] < -2208902400000, 'release_date'] = -2208988800000
-    # aqui hay que generar un hash que sea el id
-    # luego en tabla link artist_genre hay que reemplazar el name por el id
-    table['popularity'] = table['popularity']
+    table['name'] = table['id'].str.decode('utf8', errors='ignore')
+    table['id'] = abs(table['id'].str.decode('utf8').apply(hash))
+
     # """
-    table.to_csv('/home/sergio/dev/python/music-analysis-pipeline/datalake/raw/tracks.csv',
+    table.to_csv('/home/sergio/dev/python/music-analysis-pipeline/datalake/raw/genres.csv',
                  index=False, encoding='utf-8')
 
 
--- src/genres/pyspark/genres.py
+++ src/genres/pyspark/genres.py
@@ -5,13 +5,6 @@ from pyspark.sql.types import StructType, StructField, StringType, IntegerType, 
 import os
 import datetime
 
-"""
-def get_current_datetime():
-    now = datetime.datetime.now()
-    return '123456'
-udf_get_date = func.udf(get_current_datetime, returnType=StringType())
-"""
-
 if __name__ == "__main__":
     # sc = SparkContext(appName="CSV2Parquet")
     # sqlContext = SQLContext(sc)
@@ -21,10 +14,6 @@ if __name__ == "__main__":
     schema = StructType([
         StructField("id", StringType(), True),
         StructField("name", StringType(), True),
-        StructField("album_group", StringType(), True),
-        StructField("album_type", StringType(), True),
-        StructField("release_date", StringType(), True),
-        StructField("popularity", DecimalType(12, 0), True)
     ])
 
     dirname = os.path.dirname(os.path.abspath(__file__))
@@ -34,31 +23,15 @@ if __name__ == "__main__":
     df = spark.read.option("header", True).format("csv").schema(schema).option("delimiter", ',').load(
         "/home/sergio/dev/python/music"
         "-analysis-pipeline/datalake"
-        "/raw/tracks.csv")
+        "/raw/genres.csv")
 
-    # change column type
-    """
-    ERROR Utils: Aborting task
-    org.apache.spark.SparkUpgradeException: You may get a different result due to the upgrading of Spark 3.0: 
-    writing dates before 1582-10-15 or timestamps before 1900-01-01T00:00:00Z into Parquet INT96
-    files can be dangerous, as the files may be read by Spark 2.x or legacy versions of Hive
-    later, which uses a legacy hybrid calendar that is different from Spark 3.0+'s Proleptic
-    Gregorian calendar. See more details in SPARK-31404. You can set spark.sql.parquet.int96RebaseModeInWrite to 'LEGACY' to
-    rebase the datetime values w.r.t. the calendar difference during writing, to get maximum
-    interoperability. Or set spark.sql.parquet.int96RebaseModeInWrite to 'CORRECTED' to write the datetime values as it is,
-    if you are 100% sure that the written files will only be read by Spark 3.0+ or other
-    systems that use Proleptic Gregorian calendar.    
-    """
 
     df = df.withColumn('process_date',
                        func.lit(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")).cast(TimestampType()))
-    df = df.withColumn("_release_date", func.to_timestamp(func.col("release_date") / 1000).cast(TimestampType()))
-    df = df.drop('release_date')
-    df = df.withColumnRenamed("_release_date", "release_date")
 
     parquetfilename = os.path.join(dirname, 'output.parquet')
 
     df.write.mode('overwrite').parquet(
-        '/home/sergio/dev/python/music-analysis-pipeline/datalake/parquet/tracks.parquet')
+        '/home/sergio/dev/python/music-analysis-pipeline/datalake/parquet/genres.parquet')
 
     df.show()
