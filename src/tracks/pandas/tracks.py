37c861ce47bad8f61ebb97e6e8a729594d0fc9c3
sergiojulio <sergiojulio@gmail.com>
2023-11-08 21:05:58 -0300

    comments removed

--- src/tracks/pandas/tracks.py
+++ src/tracks/pandas/tracks.py
@@ -1,18 +1,12 @@
 import sqlite3
 import pandas as pd
 
-# env path db, 
-# path raw file name, 
-# arg table, 
-
 
 def pandas_sqlite_csv():
     conn = sqlite3.connect('/home/sergio/dev/python/music-analysis-pipeline/inputs/spotify.sqlite')
-    # Could not decode to UTF-8 column 'name'
+
     conn.text_factory = bytes
-    # cursor = conn.cursor()
-    # weirdos chars
-    # conn.text_factory = str
+
     table = pd.read_sql('SELECT id, disc_number, duration, explicit, audio_feature_id, name, preview_url, '
                         'track_number, popularity, is_playable FROM tracks', conn)
 
