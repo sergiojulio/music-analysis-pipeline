47959928a6c833ad1e130cca682f34831043e3ef
sergiojulio <sergiojulio@gmail.com>
2023-11-17 21:11:43 -0300

    audio feature dag added

---
+++ airflow/dags/tables/audio_feature.py
@@ -0,0 +1,34 @@
+from airflow.models import DAG
+from airflow.operators.bash import BashOperator
+from airflow.utils.dates import days_ago
+
+
+args = {
+    'owner': 'Serginho',
+    'start_date': days_ago(1)
+}
+
+# defining the dag object
+dag = DAG(
+    dag_id='extract-audio_featura',
+    default_args=args,
+    schedule_interval=None
+)
+
+with dag:
+    step1 = BashOperator(
+                task_id='step1', 
+                bash_command='source /home/sergio/dev/python/music-analysis-pipeline/.env/bin/activate && python '
+                             '/home/sergio/dev/python/music-analysis-pipeline/src/audio_featura/pandas/audio_featura.py',
+            )
+    
+    step2 = BashOperator(
+                task_id='step2', 
+                bash_command="""
+                                export JAVA_HOME="/usr/lib/jvm/java-11-openjdk-amd64/";
+                                export PATH=$PATH:$JAVA_HOME/bin;
+                                spark-submit /home/sergio/dev/python/music-analysis-pipeline/src/audio_featura/pyspark/audio_featura.py
+                            """,
+            )
+
+step1 >> step2
