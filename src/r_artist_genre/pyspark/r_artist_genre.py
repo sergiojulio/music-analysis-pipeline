20e40d880dd14fbc3d85b6f14b2491e69b51c346
sergiojulio <sergiojulio@gmail.com>
2023-12-08 20:49:39 -0300

    r_artist_genre files added

---
+++ src/r_artist_genre/pyspark/r_artist_genre.py
@@ -0,0 +1,32 @@
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as func
+from pyspark.sql.functions import lit
+from pyspark.sql.types import StructType, StructField, ByteType, BooleanType, ByteType, DoubleType, StringType, IntegerType, TimestampType
+import os
+import datetime
+
+if __name__ == "__main__":
+
+    spark = SparkSession.builder.appName("homologacion").getOrCreate()
+    sc = spark.sparkContext
+
+    schema = StructType([
+        StructField("album_id", StringType(), True),
+        StructField("track_id", StringType(), True)
+    ])
+
+    dirname = os.path.dirname(os.path.abspath(__file__))
+
+    df = spark.read.option("header", True).format("csv").schema(schema).option("delimiter", ',').load(
+        "/home/sergio/dev/python/music"
+        "-analysis-pipeline/datalake"
+        "/raw/r_albums_tracks.csv")
+
+    df = df.withColumn('process_date',
+                       func.lit(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")).cast(TimestampType()))
+
+    parquetfilename = os.path.join(dirname, 'output.parquet')
+
+    df.write.mode('overwrite').parquet(
+        '/home/sergio/dev/python/music-analysis-pipeline/datalake/parquet/r_albums_tracks.parquet')
+
