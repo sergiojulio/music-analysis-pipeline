f420179354b751757e83c8fba84b0a755c3bb272
sergiojulio <sergiojulio@gmail.com>
2023-12-09 10:00:36 -0300

    r_track_artist files added

---
+++ src/r_track_artist/pandas/r_track_artist.py
@@ -0,0 +1,20 @@
+import sqlite3
+import pandas as pd
+
+
+def pandas_sqlite_csv():
+    conn = sqlite3.connect('/home/sergio/dev/python/music-analysis-pipeline/inputs/spotify.sqlite')
+
+    # conn.text_factory = bytes
+
+    # delete cvs file if exists
+
+    for chunk in pd.read_sql('SELECT album_id, track_id FROM r_albums_tracks', conn, chunksize=100000):
+        # chunk['id'] = chunk['id'].str.decode('utf8', errors='ignore')
+
+        chunk.to_csv('/home/sergio/dev/python/music-analysis-pipeline/datalake/raw/r_albums_tracks.csv',
+                     index=False, encoding='utf-8', mode='a')  # be carefoul with a mode, its adds row to current file
+
+
+if __name__ == '__main__':
+    pandas_sqlite_csv()
