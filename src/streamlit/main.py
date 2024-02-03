adcc0e6980c30732c1bf701363d3e5144760b729
sergiojulio <sergiojulio@gmail.com>
2024-02-03 18:41:08 -0300

    streamlit changes

--- src/streamlit/main.py
+++ src/streamlit/main.py
@@ -4,7 +4,7 @@ import duckdb
 con = duckdb.connect(database='/home/sergio/dev/python/music-analysis-pipeline/datalake/db.duckdb', read_only=True)
 
 st.set_page_config(layout="wide")
-
+st.header("Filter and edit data")
 st.title('Duckdb + Streamlit')
 
 df = con.execute("""
@@ -14,3 +14,9 @@ df = con.execute("""
     """).df()
 
 st.table(df)
+
+st.button("Reset", type="primary")
+if st.button('Say hello'):
+    st.write('Why hello there')
+else:
+    st.write('Goodbye')
